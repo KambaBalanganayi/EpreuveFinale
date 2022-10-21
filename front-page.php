ae633210e039d3e077755bb6cdfccb61dc76b91e
Kamba Balanganayi <79821137+KambaBalanganayi@users.noreply.github.com>
2022-10-21 03:01:29 -0400

    changer la taille du h1 pour un h2 dans le front-page.php

--- front-page.php
+++ front-page.php
@@ -23,7 +23,7 @@
                 while( have_posts() ) :
                 the_post();
         ?>
-                <h1><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h1>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
         <?php
                 the_content(null, true);
                 endwhile;
