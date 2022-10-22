a85ac3b0095abfad5cd33648d8b5125585dfb195
Kamba Balanganayi <79821137+KambaBalanganayi@users.noreply.github.com>
2022-10-22 00:15:08 -0400

    changement de sidebar-content.scss pour ajouter un media query ainsi que des selecteurs pour rendre le menu responsive c-8

--- functions.php
+++ functions.php
@@ -70,3 +70,22 @@ function kamba_theme_register_nav_menu(){
     ) );
 }
 add_action('after_setup_theme', 'kamba_theme_register_nav_menu', 0);
+
+
+/*---------------------Definition du filtre pour les elements du menu sidebar-----------------*/
+
+function igc31w_filtre_choix_menu($obj_menu, $arg){
+ 
+    if ($arg->menu == "sidebar"){
+    foreach($obj_menu as $cle => $value)
+    {
+      //  print_r($value);
+       //$value->title = substr($value->title,7);
+       $value->title = wp_trim_words($value->title,3,"...");
+        //echo $value->title . '<br>';
+     } 
+    }
+    //die();
+    return $obj_menu;
+}
+add_filter("wp_nav_menu_objects","igc31w_filtre_choix_menu", 10,2);
