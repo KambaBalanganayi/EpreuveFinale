f7babab52c61edbe5bfa8bee79c952d9aa99d232
Kamba Balanganayi <79821137+KambaBalanganayi@users.noreply.github.com>
2022-10-22 03:15:03 -0400

    remanié la fonction qui reduit la taille des items du menu du sidebar

--- functions.php
+++ functions.php
@@ -76,16 +76,13 @@ add_action('after_setup_theme', 'kamba_theme_register_nav_menu', 0);
 
 function igc31w_filtre_choix_menu($obj_menu, $arg){
  
-    if ($arg->menu == "sidebar"){
+    if ($arg->menu == "aside"){
     foreach($obj_menu as $cle => $value)
     {
-      //  print_r($value);
-       //$value->title = substr($value->title,7);
+       $value->title = substr($value->title,7);
        $value->title = wp_trim_words($value->title,3,"...");
-        //echo $value->title . '<br>';
      } 
     }
-    //die();
     return $obj_menu;
 }
 add_filter("wp_nav_menu_objects","igc31w_filtre_choix_menu", 10,2);
